3390ed274007595b1eec20bad18b5809ee2af79d
Denis <77270222+ayonovdenizs@users.noreply.github.com>
2024-10-17 19:09:19 +0600

    Load original string's

---
+++ en.py
@@ -0,0 +1,25 @@
+grettigs = """
+<a href='https://raw.githubusercontent.com/ayonovdenizs/ayonovdenizs.github.io/refs/heads/main/Picsart_24-10-13_11-46-30-402.jpg'>👀</a>Welcome to ChatGPT by ayonovdenizs!
+
+To send a message, simply write to the bot. You can also support the project (/donate)
+
+Be sure to subscribe to the channel!
+"""
+channel_button_text = 'Go to channel'
+support_button_text = 'Technical support'
+donate_msg_grettings = 'Thank you for the donation. Donators get special perks'
+setiings_provider = 'AI model Switching Menu\nSelect a model from the list:'
+ban_msg_user = 'You are banned. Reason: reason_replace'
+unban_msg_user = 'You are unbanned.'
+give_donate_user = 'You have received a donation privilege'
+faq_msg = 'FAQ on errors'
+error_msg = 'An error occurred!: error_replace'
+thanks_donate = ''
+waiting_msg = 'Generating... Please wait...'
+clear_context_msg = 'Context cleared!'
+error_toobigmsg = 'The response from the neural network exceeded the limits of Telegram, sending in txt format'
+clear_context_button = 'Clear context'
+cryptopaymsg = "The payment invoice is created. Follow the link below to pay. After paying, click the 'Check pay' button."
+change_provider_button = 'Change AI model'
+change_lang_text = 'Change language'
+dev_sended_message = 'The developer sent you a message. To reply, type /sendmsg <and your text>. \n Message text:'
